2f4bb31d5ea227a23eac4575ec9fb5b83db81977
Larissa Lumi <larissalumih@gmail.com>
2025-02-10 21:13:36 -0300

    estradas escuras consertada

--- 1152-Estradas-Escuras/1152.c
+++ 1152-Estradas-Escuras/1152.c
@@ -1,58 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct {
     unsigned u, v, w;
 } vertice;
 
 vertice g[200100];
-int p[2000100];
+int p[200100];
 
-int pai(int i){
-    if(i == p[i])
-        return i;
-    return pai(p[i]);
+int pai(int i) {
+    if(i != p[i])
+        p[i] = pai(p[i]);  // Compressão de caminho
+    return p[i];
 }
 
-int kruskal(int c){
-    int i, v, u;
-    unsigned resp;
+int kruskal(int numArestas) {
+    int i, raizU, raizV;
+    unsigned int pesoMST = 0;
 
-    for (i = 0, resp = 0; i < c; ++i){
-        v = pai(g[i].v);
-        u = pai(g[i].u);
+    for (i = 0; i < numArestas; ++i) {
+        raizU = pai(g[i].u);
+        raizV = pai(g[i].v);
 
-        if(u != v)
-            p[v] = p[u], resp += g[i].w;
+        if (raizU != raizV) {
+            p[raizV] = raizU;
+            pesoMST += g[i].w;
+        }
     }
 
-    return resp;
+    return pesoMST;
 }
 
-int compara(vertice *a, vertice *b){
-    return a->w - b->w;
+int compara(const void *a, const void *b) {
+    return ((vertice *)a)->w - ((vertice *)b)->w;
 }
 
-int main(){
-    int e, v;
+int main() {
+    int numVertices, numArestas;
 
-    while(scanf("%d %d", &e, &v), e && v){
+    while (scanf("%d %d", &numVertices, &numArestas), numVertices && numArestas) {
         int i;
-        unsigned total = 0;
-        for (i = 0; i < v; ++i){
-            scanf("%d %d %d", &g[i].v, &g[i].v, &g[i].u, &g[i].w), total += g[i].w;
+        unsigned int pesoTotal = 0;
+
+        for (i = 0; i < numArestas; ++i) {
+            scanf("%d %d %d", &g[i].u, &g[i].v, &g[i].w);
+            pesoTotal += g[i].w;
         }
 
-        qsort(g, v, sizeof(vertice), compara);
+        qsort(g, numArestas, sizeof(vertice), compara);
 
-        for (i = 1; i <= e; ++i)
+        for (i = 0; i < numVertices; ++i)
             p[i] = i;
 
-        printf("%u\n", total - kruskal(v));
-
-        memset(g, 0, sizeof(g));
-        memset(p, 0, sizeof(p));
+        printf("%u\n", pesoTotal - kruskal(numArestas));
     }
 
     return 0;
